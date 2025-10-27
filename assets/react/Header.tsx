23f6bc03ad38446c22e20def9915429a6227ab46
Sakhooo71 <144905856+Sakhooo71@users.noreply.github.com>
2025-10-27 23:05:20 +0100

    création de 'Header.tsx'

---
+++ assets/react/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {AppBar, Badge, Grid, IconButton, Toolbar} from '@mui/material';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+
+const calculateTotalQuantity = ( ) => {
+    return (
+        <
+    )
+}
