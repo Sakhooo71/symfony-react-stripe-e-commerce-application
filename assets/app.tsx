24f8527250cf5dc342098383dbd4636beb66a538
Sakhooo71 <144905856+Sakhooo71@users.noreply.github.com>
2025-10-27 23:05:57 +0100

    update de "app.tsx"

---
+++ assets/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import RectDom from 'react-dom/client';
+import { registerReactControllerComponents } from '@symfony/ux-react';
+import './bootstrap.js';
+import './styles/app.css';
+
+console.log('Hello from app.tsx!');
